d40794ed0f7f4e3f9c505a0157577542cb247ef1
Raghendra kumar <50679225+raghvendra98@users.noreply.github.com>
2021-01-08 19:58:48 +0530

    Feat: trackbar to set font color on image

--- trackback.py
+++ trackback.py
@@ -5,31 +5,30 @@ def nothing(x):
     print(x)
 
 # Create a black image, a window
-img = np.zeros((300,512,3), np.uint8)
 cv.namedWindow('image')
 
-cv.createTrackbar('B', 'image', 0, 255, nothing)
-cv.createTrackbar('G', 'image', 0, 255, nothing)
-cv.createTrackbar('R', 'image', 0, 255, nothing)
+cv.createTrackbar('CP', 'image', 10, 400, nothing)
 
-switch = '0 : OFF\n 1 : ON'
+switch = 'color/gray'
 cv.createTrackbar(switch, 'image', 0, 1, nothing)
 
 while(1):
-    cv.imshow('image',img)
+    img = cv.imread('trex.png')
+    pos = cv.getTrackbarPos('CP', 'image')
+    font = cv.FONT_HERSHEY_SIMPLEX
+    cv.putText(img, str(pos), (50, 150), font, 3, (0, 0, 255), 10)
+
     k = cv.waitKey(1) & 0xFF
     if k == 27:
         break
 
-    b = cv.getTrackbarPos('B', 'image')
-    g = cv.getTrackbarPos('G', 'image')
-    r = cv.getTrackbarPos('R', 'image')
     s = cv.getTrackbarPos(switch, 'image')
 
     if s == 0:
-       img[:] = 0
+       pass
     else:
-       img[:] = [b, g, r]
+       img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+    img = cv.imshow('image',img)
 
 cv.destroyAllWindows()
